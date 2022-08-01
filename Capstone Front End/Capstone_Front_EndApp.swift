850b9a7dcebb7a8f3a9248634eeb0c8abdb5e245
Joanna Dudley <jfdudleybmc@gmail.com>
2022-08-01 16:20:57 -0400

    Initial Commit

---
+++ Capstone Front End/Capstone_Front_EndApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capstone_Front_EndApp.swift
+//  Capstone Front End
+//
+//  Created by Joanna Dudley on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Capstone_Front_EndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capstone Front End/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Capstone Front End
+//
+//  Created by Joanna Dudley on 8/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
